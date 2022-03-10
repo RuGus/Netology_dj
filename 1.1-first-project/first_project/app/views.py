e70070f8c083460b7d5ba513c473ad8f53221be8
Ruslan Gusmanov <rgusmanov@turbozaim.ru>
2022-03-10 11:56:08 +0300

    Solved first project

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,35 +1,46 @@
+import os
+from datetime import datetime
+
 from django.http import HttpResponse
-from django.shortcuts import render, reverse
+from django.shortcuts import render
+from django.urls import reverse
 
 
 def home_view(request):
-    template_name = 'app/home.html'
+    template_name = "app/home.html"
     # впишите правильные адреса страниц, используя
     # функцию `reverse`
     pages = {
-        'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        "Главная страница": reverse("home"),
+        "Показать текущее время": reverse("time"),
+        "Показать содержимое рабочей директории": reverse("workdir"),
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
-    context = {
-        'pages': pages
-    }
+    context = {"pages": pages}
     return render(request, template_name, context)
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
+    # обратите внимание – здесь HTML шаблона нет,
     # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
+    current_time = datetime.now().time()
+    msg = f"Текущее время: {current_time}"
     return HttpResponse(msg)
 
 
 def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
+    # который возвращает список файлов в рабочей
     # директории
-    raise NotImplemented
+    files_list = "<br/>"
+    for dirpath, dirnames, filenames in os.walk("."):
+        # перебрать каталоги
+        for dirname in dirnames:
+            files_list += f"Каталог: {os.path.join(dirpath, dirname)} <br/>"
+        # перебрать файлы
+        for filename in filenames:
+            files_list += f"Файл: {os.path.join(dirpath, filename)} <br/>"
+    msg = f"В рабочей директории размещены следующие файлы и каталоги: {files_list}"
+    return HttpResponse(msg)
