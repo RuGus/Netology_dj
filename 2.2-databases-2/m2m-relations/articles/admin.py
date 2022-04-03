16a6a1b7dddc8049ed70aa6f0a89a9abdf797276
Ruslan Gusmanov <rgusmanov@turbozaim.ru>
2022-04-03 23:12:24 +0300

    Solved m2m-relations

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, ArticleScope
+
+
+class ArticleScopeFormset(BaseInlineFormSet):
+    def clean(self):
+        main_count = 0
+        for form in self.forms:
+            forms_dict = form.cleaned_data
+            if forms_dict.get("is_main"):
+                main_count += 1
+            if main_count > 1:
+                raise ValidationError("У статьи может быть только 1 основной раздел")
+        if main_count == 0:
+            raise ValidationError("У статьи должен быть хотя бы 1 основной раздел")
+        return super().clean()
+
+
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    extra = 5
+    formset = ArticleScopeFormset
 
 
 @admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleScopeInline]
+
+
+@admin.register(Scope)
 class ArticleAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -3,14 +3,43 @@ from django.db import models
 
 class Article(models.Model):
 
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст')
-    published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    title = models.CharField(max_length=256, verbose_name="Название")
+    text = models.TextField(verbose_name="Текст")
+    published_at = models.DateTimeField(verbose_name="Дата публикации")
+    image = models.ImageField(
+        null=True,
+        blank=True,
+        verbose_name="Изображение",
+    )
 
     class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+        ordering = ["-published_at"]
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название")
+    article = models.ManyToManyField(
+        Article, through="ArticleScope", verbose_name="Статья"
+    )
+
+    class Meta:
+        verbose_name = "Раздел"
+        verbose_name_plural = "Разделы"
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleScope(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="scopes", verbose_name="Статья"
+    )
+    tag = models.ForeignKey(
+        Scope, on_delete=models.CASCADE, related_name="articles", verbose_name="Раздел"
+    )
+    is_main = models.BooleanField(verbose_name="Основной")
